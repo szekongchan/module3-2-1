c479dd7defd385e34101a7d1ac1314fb467d662f
Chan Sze Kong <szekong@gmail.com>
2026-05-07 19:29:50 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate" # Change this
+    key    = ""  # Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =   # Set your bucket name here
+}
+
+
