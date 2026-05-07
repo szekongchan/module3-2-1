806039c658c934973b776206e379c2d551e1a8d7
Chan Sze Kong <szekong@gmail.com>
2026-05-07 19:44:56 +0800

    fix tflint issues

--- main.tf
+++ main.tf
@@ -1,17 +1,28 @@
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.31.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate" # Change this
-    key    = "sk-module3-2-1"  # Change this
+    bucket = "sctp-ce12-tfstate-bucket" # Change this
+    key    = "sk-module3-2-1"    # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =   # Set your bucket name here
+  bucket_prefix = "sk" # Set your bucket name here
 }
 
 
